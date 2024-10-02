d8a1c8ffbe422159cdc56307a17d2734c3c73e36
Namdh157 <127576912+Namdh157@users.noreply.github.com>
2024-10-02 15:36:52 +0700

    migrate Database

---
+++ app/Models/Review.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    const RATING = [1, 2, 3, 4, 5];
+}
---
+++ database/migrations/2024_10_02_063604_create_reviews_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Review;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products');
+            $table->boolean('is_reviewed')->default(false);
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->enum('rating', array_values(Review::RATING));
+            $table->text('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
